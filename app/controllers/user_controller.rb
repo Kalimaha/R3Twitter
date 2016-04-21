2f0c4e603aa467f59af6176fc894b6af3328f113
Kalimaha <guido.barbaglia@gmail.com>
2016-04-21 10:29:12 +1000

    Login/register flow.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,6 +4,8 @@ class UserController < ApplicationController
 
   attr_reader :redis
 
+  skip_before_filter :verify_authenticity_token
+
   def initialize
     super
     @redis = Redis.new
@@ -40,6 +42,40 @@ class UserController < ApplicationController
 
   def get_user(user_id)
     @redis.hgetall(user_id)
+  end
+
+  def login
+
+    # Check input parameters.
+    if params[:username].length < 1
+      flash[:error] = 'Please provide your username.'
+      redirect_to '/'
+    elsif params[:password].length < 1
+      flash[:error] = 'Please provide your password.'
+      redirect_to '/'
+    else
+      puts params[:username]
+      puts exists?(params[:username])
+      if exists?(params[:username])
+        @user = get_user(get_user_id(params[:username]))
+      else
+        flash[:error] = "User <i>#{params[:username]}</i> does NOT exists. Please register."
+        redirect_to '/'
+      end
+    end
+
+  end
+
+  def register
+    if params[:new_password] != params[:confirm_password]
+      flash[:error] = 'Passwords do not match.'
+      redirect_to '/'
+    end
+    user = Hash.new
+    user['username'] = params[:new_username]
+    user['password'] = params[:new_password]
+    flash[:success] = 'Registration complete, please login.'
+    redirect_to '/'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   # Root URL
   root 'user#index'
 
+  # Login route
+  post '/login', to: 'user#login'
+
+  # Registration rout
+  post '/register', to: 'user#register'
+
 end
