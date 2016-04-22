052f1a7e2b9be90a961f64d8e23129c9452154fe
Kalimaha <guido.barbaglia@gmail.com>
2016-04-22 11:25:33 +1000

    Tweet helper functions.

--- app/helpers/tweet_helper.rb
+++ app/helpers/tweet_helper.rb
@@ -1,2 +1,22 @@
 module TweetHelper
+
+  def highlight(message)
+    highlight_hashtags(message)
+    highlight_at(message)
+  end
+
+  private
+  def highlight_hashtags(message)
+    tags = message.scan(/#\w+/).flatten
+    tags.each {|t| message.gsub!(t, '<a target="_blank" href="https://en.wikipedia.org/w/index.php?search=' + t[1..-1] + '">' + t + '</a>') }
+    message
+  end
+
+  private
+  def highlight_at(message)
+    tags = message.scan(/@\w+/).flatten
+    tags.each {|t| message.gsub!(t, '<a href="/tweets/' + t[1..-1] + '">' + t + '</a>') }
+    message
+  end
+
 end
